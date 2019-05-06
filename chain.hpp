4584c20a2b1d682f1a0e62f2ac19fd4db31b3dc6
Dvirhac <dvirhacohen@gmail.com>
2019-05-06 19:46:34 +0300

    first commit

--- chain.hpp
+++ chain.hpp
@@ -5,10 +5,67 @@
 #ifndef CPP_PROJECT5_CHAIN_HPP
 #define CPP_PROJECT5_CHAIN_HPP
 
+#include "range.hpp"
+namespace itertools {
+    template <typename range>
+    class chain {
+        typedef range* pointer;
+    private:
+        range r1;
+        range r2;
+    public:
 
-class chain {
+        chain(range r1 , range r2)
+                :r1(r1),r2(r2){}
 
-};
+        // begin of class iterator
+
+        class iterator {
+        private:
+            pointer ptr;
+        public:
+            iterator(pointer ptr = nullptr)
+                    : ptr(ptr) {}
+
+            range& operator*() const {
+                return
+            }
+
+            iterator& operator++() {
+                ptr++;
+                return *this;
+            }
+
+            const iterator operator++(int) {
+                iterator tmp = *this;
+                ptr++;
+                return tmp;
+
+            }
+
+            bool operator==(const iterator &it) const {
+                return ptr == it.ptr;
+            }
+
+            bool operator!=(const iterator& it) const {
+                return ptr != it.ptr;
+            }
+
+        };
+
+        //end of class iterator
+
+        iterator begin() {
+            return iterator{&r1};
+        }
+        iterator end() {
+            return iterator{&r2};
+        }
+
+
+    };
+
+}
 
 
 #endif //CPP_PROJECT5_CHAIN_HPP
--- range.hpp
+++ range.hpp
@@ -12,54 +12,63 @@
 
 namespace itertools {
     using namespace std;
-    template <typename T>
 
-    class range : public std::iterator<std::output_iterator_tag, T>
-    {
-    private:
-        T arr[2];
+    template<typename T>
+
+    class range {
+
+        typedef T* pointer;
+
     public:
+        T a, b;
+
         range(T a, T b)
-        {
-            arr[0] = a;
-            arr[1] = b;
+                :a(a),b(b){}
+
+        class iterator {
+        private:
+            pointer ptr;
+        public:
+            iterator(pointer ptr = nullptr)
+                    : ptr(ptr) {}
+
+            T& operator*() const {
+                return *ptr;
+            }
+
+            iterator& operator++() {
+                (*ptr)++;
+                return *this;
+            }
+
+            const iterator operator++(int) {
+                iterator tmp = *this;
+                ptr++;
+                return tmp;
+
+            }
+
+            bool operator==(const iterator &it) const {
+                return *ptr == *it.ptr;
+            }
+
+            bool operator!=(const iterator& it) const {
+                return *ptr != *it.ptr;
+            }
+
+        };
+
+        //end of class iterator
+
+        iterator begin() {
+            return iterator{&a};
+        }
+        iterator end() {
+            return iterator{&b};
         }
 
-        T operator*() const{
-            return *this;
-        }
 
-        range & operator++(){
-
-        }
-        range operator++(T) {
-
-        }
-
-        iterator begin();
-        iterator end();
     };
-
-
-
-    template <typename T>
-    list<T> range(T a, T b)
-    {
-
-        /*list<T> listOfElements;
-        auto it  = listOfElements.begin();
-        for (auto i = a; i < b; i++)
-        {
-            listOfElements.insert(it,i);
-            it++;
-        }
-
-        return listOfElements;
-         */
-    }
-
-
-
 }
 
 
